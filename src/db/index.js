e8e42ffc4c05f5ff2db225824fa5b9c74a978ff2
trilochan work <trilochanwork@TRILOCHANPRADASDs-MacBook-Pro.local>
2024-06-02 19:21:30 +0530

    utils , central handling of error,reponse etc

--- src/db/index.js
+++ src/db/index.js
@@ -4,11 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB  = async () =>{
     try {
       const connectionInstance  =  await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}}`);
-            console.log('mongodb connected:', connectionInstance.connection.host);
+         console.log('db msg -> mongodb connected:', connectionInstance.connection.host); 
     } catch (error) {
-        console.log(" mongodb connection failed:", error);
+        console.log(" db msg -> mongodb connection failed:", error);
         process.exit(1)  //read more on this 
     }
 }
 
-export default connectDB 
+export default connectDB
