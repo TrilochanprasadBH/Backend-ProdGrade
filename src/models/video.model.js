38adcffafbce1062b55ec7772fbd1aee6d671fc9
trilochan work <trilochanwork@TRILOCHANPRADASDs-MacBook-Pro.local>
2024-06-03 23:22:43 +0530

    video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,7 +1,38 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-    
+    videoFile:{
+        type:String,  //cloudinary link 
+        required:true
+    },
+    thumbnail:{
+        type:String,
+        required: [true, "Thumbnail is needed"]   //writing like this is upto me, or just write true 
+    },
+    owner:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    duration:{
+        type:Number,
+        required:true
+    },
+    views:{
+        type:Number,
+        default:0 
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    }
 
 },{timestamps:true})
 
